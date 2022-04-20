5eebe2c031416c13b1d6db8dfc6d4be2f040ef0a
Luiz Claro <lfclaro@gmail.com>
2022-04-20 05:32:12 -0400

    ResultView

---
+++ shelfie/Views/ResultView.swift
@@ -0,0 +1,25 @@
+//
+//  ResultView.swift
+//  shelfie
+//
+//  Created by Luiz Fernando Reis on 2022-04-20.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        ZStack{
+            Image("bg").resizable().edgesIgnoringSafeArea(.all)
+            Text("Games detected in your SHELFIE!")
+                .font(.custom("Avenir-Black", size: sf.h * 0.04))
+                .multilineTextAlignment(.center)
+        }
+    }
+}
+
+struct ResultView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResultView()
+    }
+}
